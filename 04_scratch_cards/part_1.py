ac71905493b6bc53c7c55f31434f8bb7566eb2e2
Jonny Brownrigg <jonny-uk@hotmail.com>
2023-12-04 23:09:03 +0000

    completed day 4 part 1

---
+++ 04_scratch_cards/part_1.py
@@ -0,0 +1,16 @@
+def getWinningValue(line):
+    winnersCount = 0
+    winningNumbers = line.split("|")[0].split(":")[1].split()
+    myNumbers = line.split("|")[1].split()
+    winnersCount = len([number for number in myNumbers if number in winningNumbers])
+    if winnersCount > 0:
+        return pow(2, (winnersCount - 1))
+    else:
+        return 0
+
+
+with open("input.txt", "r") as file:
+    sum = 0
+    for line in file:
+        sum += getWinningValue(line)
+    print(sum)
